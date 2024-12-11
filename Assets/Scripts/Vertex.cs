f1f1b4f8f1b0d68eae562ce2ecd6752c7e24e1a9
Benjamin Wiemann <benjamin.wiemann@posteo.de>
2024-12-11 20:29:35 +0100

    First working version of triplanar mapping
    Former-commit-id: bf510505ba3879ad78e82a33a536f3b5ada06d7c

--- Assets/Scripts/Vertex.cs
+++ Assets/Scripts/Vertex.cs
@@ -7,7 +7,7 @@ namespace LiquidPlanet
     {
         public float3 position;        
         public half4 tangent;
-        public half3 bitangent;
+        public float3 normal;
         public float2 texCoord0;
     }
 
