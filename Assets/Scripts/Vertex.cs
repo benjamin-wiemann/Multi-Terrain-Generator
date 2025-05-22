d1b0b4d1bd68b37e826ec7ff8db8631f293942fc
Benjamin Wiemann <benjamin.wiemann@posteo.de>
2025-05-22 17:34:01 +0200

    Shader debug views added

--- Assets/Scripts/Vertex.cs
+++ Assets/Scripts/Vertex.cs
@@ -5,9 +5,9 @@ namespace MultiTerrain
 
     public struct Vertex
     {
-        public float3 position;        
+        public float3 position;
+        public float3 normal;        
         public half4 tangent;
-        public float3 normal;
         public float2 texCoord0;
     }
 
